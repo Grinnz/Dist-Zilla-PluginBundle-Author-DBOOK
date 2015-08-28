20e2fed7a1d85a4711070dc65ad9711a238152f0
Dan Book <grinnz@grinnz.com>
2015-08-28 15:25:07 -0400

    Bump version

--- lib/Dist/Zilla/MintingProfile/Author/DBOOK.pm
+++ lib/Dist/Zilla/MintingProfile/Author/DBOOK.pm
@@ -3,7 +3,7 @@ package Dist::Zilla::MintingProfile::Author::DBOOK;
 use Moose;
 with 'Dist::Zilla::Role::MintingProfile::ShareDir';
 
-our $VERSION = '0.013';
+our $VERSION = '0.014';
 
 1;
 
--- lib/Dist/Zilla/PluginBundle/Author/DBOOK.pm
+++ lib/Dist/Zilla/PluginBundle/Author/DBOOK.pm
@@ -4,7 +4,7 @@ use Moose;
 use Scalar::Util 'blessed';
 with 'Dist::Zilla::Role::PluginBundle::Easy', 'Dist::Zilla::Role::PluginBundle::Config::Slicer';
 
-our $VERSION = '0.013';
+our $VERSION = '0.014';
 
 sub configure {
 	my $self = shift;
