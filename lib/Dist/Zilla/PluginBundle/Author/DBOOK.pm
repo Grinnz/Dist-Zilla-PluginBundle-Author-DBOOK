cfb34ec64ccbe355d641ffda459a608750028396
Dan Book <grinnz@grinnz.com>
2016-09-29 14:05:31 -0400

    drop version, regenerate meta files

--- lib/Dist/Zilla/PluginBundle/Author/DBOOK.pm
+++ lib/Dist/Zilla/PluginBundle/Author/DBOOK.pm
@@ -8,7 +8,7 @@ with 'Dist::Zilla::Role::PluginBundle::Easy',
 use namespace::clean;
 use Data::Section -setup;
 
-our $VERSION = '0.029';
+our $VERSION = '0.028';
 
 sub configure {
 	my $self = shift;
