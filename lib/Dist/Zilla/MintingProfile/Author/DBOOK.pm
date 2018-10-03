51eee2dca0b5367932ccd711bdefb1ce58cea92a
Dan Book <grinnz@grinnz.com>
2018-10-03 13:34:43 -0400

    Bump version

--- lib/Dist/Zilla/MintingProfile/Author/DBOOK.pm
+++ lib/Dist/Zilla/MintingProfile/Author/DBOOK.pm
@@ -4,7 +4,7 @@ use Moose;
 with 'Dist::Zilla::Role::MintingProfile::ShareDir';
 use namespace::clean;
 
-our $VERSION = '0.038';
+our $VERSION = '0.039';
 
 __PACKAGE__->meta->make_immutable;
 1;
--- lib/Dist/Zilla/PluginBundle/Author/DBOOK.pm
+++ lib/Dist/Zilla/PluginBundle/Author/DBOOK.pm
@@ -8,7 +8,7 @@ with 'Dist::Zilla::Role::PluginBundle::Easy',
 use namespace::clean;
 use Data::Section -setup;
 
-our $VERSION = '0.038';
+our $VERSION = '0.039';
 
 sub configure {
 	my $self = shift;
