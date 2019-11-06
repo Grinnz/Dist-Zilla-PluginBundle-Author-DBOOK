4bbc4ea517e341857db056d36d020a615fd34e68
Dan Book <grinnz@grinnz.com>
2019-11-06 12:38:20 -0500

    change versioning scheme

--- lib/Dist/Zilla/PluginBundle/Author/DBOOK.pm
+++ lib/Dist/Zilla/PluginBundle/Author/DBOOK.pm
@@ -8,7 +8,7 @@ with 'Dist::Zilla::Role::PluginBundle::Easy',
 use namespace::clean;
 use Data::Section -setup;
 
-our $VERSION = '0.039';
+our $VERSION = 'v1.0.0';
 
 sub configure {
 	my $self = shift;
