26152b376abd553a21f1a9cdf43d364e7ac05082
Dan Book <grinnz@grinnz.com>
2020-09-26 20:39:12 -0400

    Bump version

--- lib/Dist/Zilla/PluginBundle/Author/DBOOK.pm
+++ lib/Dist/Zilla/PluginBundle/Author/DBOOK.pm
@@ -8,7 +8,7 @@ with 'Dist::Zilla::Role::PluginBundle::Easy',
 use namespace::clean;
 use Data::Section -setup;
 
-our $VERSION = 'v1.0.4';
+our $VERSION = 'v1.0.5';
 
 sub configure {
 	my $self = shift;
